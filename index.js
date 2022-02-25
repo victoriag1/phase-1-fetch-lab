e693b03e38fd5e468a59d9c2ff35dd9f9cf10831
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-24 19:11:35 -0800

    commit tree 825594da29010f408bbc4249b8000e15d85ef9b7

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+const apiBooks = 'https://anapioficeandfire.com/api/books'
+
+const fetchBooks = fetch(apiBooks)
+.then((resp)=> resp.json())
+.then((json)=> {
+  renderBooks(json)
+})
+
+
+function renderBooks() {
+
+}
+// const fetchBooks = function fetchBooks(){
   
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// }
